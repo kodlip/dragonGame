df40f469e9827d6fce6c23f6d24f3327055e23f3
kodlip <154709407+kodlip@users.noreply.github.com>
2024-03-19 15:44:35 +0300

    q

---
+++ Assets/Finish.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+            GameController.Instance.LoadNextLevelScene();
+        }
+    }
+}
